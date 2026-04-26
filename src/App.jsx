61c3e896b8c4407b578123a89b039323d3603d47
priyanshi-717 <prinshupatel001@gmail.com>
2026-04-26 19:02:03 +0530

    initial commit with folder structure

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+function App() {
+  var [a,b]= useState(69);
+  return (
+    <>
+      <div className='w-full h-screen bg-zinc-700 p-4'>
+        
+      </div>
+      
+    </>
+  )
+}
+
+export default App
